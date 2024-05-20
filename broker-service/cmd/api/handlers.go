de1d36a57abf34651c36d5982f9d665eccc335e7
VedantWankhade <vedantwankhade17@gmail.com>
2024-05-20 19:09:01 +0530

    auth complete

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"net/http"
 )
@@ -32,11 +34,45 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	}
 	switch requestPayload.Action {
 	case "auth":
-
+		app.authenticate(w, requestPayload.Auth)
 	default:
 		app.errorJSON(w, errors.New("Unknown request action"))
 	}
 }
 
-func (app *Config) Authenticate(w http.ResponseWriter, a AuthPayload) {
+func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
+	jsonData, _ := json.MarshalIndent(a, "", "\t")
+	req, err := http.NewRequest(http.MethodPost, "http://auth-service/authenticate", bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer res.Body.Close()
+	if res.StatusCode == http.StatusUnauthorized {
+		app.errorJSON(w, errors.New("invalid creds"))
+		return
+	} else if res.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("invalid creds again"))
+		return
+	}
+	var jsonFromService response
+	err = json.NewDecoder(res.Body).Decode(&jsonFromService)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	if jsonFromService.Error {
+		app.errorJSON(w, errors.New("authe service returned err:"+jsonFromService.Message), http.StatusUnauthorized)
+	}
+	var payload response
+	payload.Error = false
+	payload.Message = "Authenticated!" + jsonFromService.Message
+	payload.Data = jsonFromService.Data
+	app.writeJSON(w, http.StatusAccepted, payload)
 }
